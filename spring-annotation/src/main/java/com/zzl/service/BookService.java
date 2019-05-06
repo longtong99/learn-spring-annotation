5bef4dc08da1fae22338d479687c00aa86eace37
a-zhengzilong <a-@@ZHENGZILONG>
2019-05-06 15:02:17 +0800

    自动装配~@Inject和@Resource整理完成

--- spring-annotation/src/main/java/com/zzl/service/BookService.java
+++ spring-annotation/src/main/java/com/zzl/service/BookService.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
 @Service
 public class BookService {
 
-    @Autowired(required = false)
-    @Qualifier("bookDao")
+    //@Autowired(required = false)
+    //@Qualifier("bookDao")
+    //@Resource(name = "bookDao2")
+    @Inject
     BookDao bookDao;
 
     public void print(){
